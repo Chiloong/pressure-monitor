bd02eeae2244bd2991ea5e93e0b31c2f13d6e0dd
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-06 20:49:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from wind import check_wind
 from pressure import check_pressure
+from fusion import check_fusion
 
 def main():
     check_wind()
     check_pressure()
+    check_fusion()   # ✅ 新增这一行
 
 if __name__ == "__main__":
     main()
